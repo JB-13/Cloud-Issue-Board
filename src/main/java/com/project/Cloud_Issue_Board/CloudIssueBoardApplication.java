e4d2208d24bb06cb3182bd56d3404d4999007b64
Jan <jabu1005@stud.hs-kl.de>
2025-01-07 12:46:04 +0100

    Spring Boot Projekt angelegt

---
+++ src/main/java/com/project/Cloud_Issue_Board/CloudIssueBoardApplication.java
@@ -0,0 +1,13 @@
+package com.project.Cloud_Issue_Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudIssueBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudIssueBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Cloud_Issue_Board/CloudIssueBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Cloud_Issue_Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudIssueBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
