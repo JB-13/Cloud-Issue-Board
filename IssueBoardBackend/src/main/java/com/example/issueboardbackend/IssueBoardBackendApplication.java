d060ae80f55e14a7f98f80e6f36ab21ba10f033b
Jan <jabu1005@stud.hs-kl.de>
2025-01-08 16:21:31 +0100

    Spring Boot Projekt angepasst

---
+++ IssueBoardBackend/src/main/java/com/example/issueboardbackend/IssueBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.issueboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueBoardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueBoardBackendApplication.class, args);
+    }
+
+}
