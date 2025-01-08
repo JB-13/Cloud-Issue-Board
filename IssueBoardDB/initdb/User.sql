803970375edf16589a9cadd0265ad279f0bff2bf
ESNF <ericstricker16@gmail.com>
2025-01-08 19:30:47 +0100

    passwort angepasst

--- IssueBoardDB/initdb/User.sql
+++ IssueBoardDB/initdb/User.sql
@@ -3,14 +3,15 @@ CREATE DATABASE IF NOT EXISTS issue_board;
 USE issue_board;
 
 CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    userid INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    password_salt BINARY(32) NOT NULL,
+    password_hash BINARY(20)NOT NULL,
     role VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS issues (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    issuesid INT AUTO_INCREMENT PRIMARY KEY,
     titel VARCHAR(255) NOT NULL,
     description TEXT,
     status VARCHAR(20) DEFAULT 'Open',
@@ -18,16 +19,16 @@ CREATE TABLE IF NOT EXISTS issues (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     created_by INT,
     assigned_to INT,
-    FOREIGN KEY (created_by) REFERENCES users(id),
-    FOREIGN KEY (assigned_to) REFERENCES users(id)
+    FOREIGN KEY (created_by) REFERENCES users(userid),
+    FOREIGN KEY (assigned_to) REFERENCES users(userid)
 );
 
 CREATE TABLE IF NOT EXISTS comments(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    commentsid INT AUTO_INCREMENT PRIMARY KEY,
     issue_id INT NOT NULL,
     user_id INT NOT NULL,
     comment TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (issue_id) REFERENCES issues(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (issue_id) REFERENCES issues(issuesid) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(userid)
 );
