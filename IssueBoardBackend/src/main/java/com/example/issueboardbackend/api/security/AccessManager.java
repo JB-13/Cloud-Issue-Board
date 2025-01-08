ca0d046e20982620b8828d61a0121cf90f9110a9
Jan <jabu1005@stud.hs-kl.de>
2025-01-08 18:17:49 +0100

    Erste BackendOrdner und Klassen angelegt

---
+++ IssueBoardBackend/src/main/java/com/example/issueboardbackend/api/security/AccessManager.java
@@ -0,0 +1,4 @@
+package com.example.issueboardbackend.api.security;
+
+public class AccessManager {
+}
---
+++ IssueBoardBackend/src/main/java/com/example/issueboardbackend/api/security/AccessToken.java
@@ -0,0 +1,38 @@
+package com.example.issueboardbackend.api.security;
+
+import java.util.Objects;
+
+public class AccessToken {
+    private String accessToken;
+
+    public  AccessToken(String uuid) {
+        this.accessToken = uuid;
+    }
+
+    public AccessToken() {}
+
+    public String getAccessToken()
+    {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken)
+    {
+        this.accessToken = accessToken;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        AccessToken accessToken = (AccessToken) o;
+        return Objects.equals(this.accessToken, accessToken.accessToken);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(accessToken);
+    }
+}
