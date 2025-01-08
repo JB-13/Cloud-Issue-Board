2e57bd31625308ade674436a4595c6ddb6aa03ba
Jan <jabu1005@stud.hs-kl.de>
2025-01-08 19:04:11 +0100

    IssueBoardWebServer Projektordner erstellt

---
+++ IssueBoardWebServer/src/main/java/com/example/issueboardwebserver/IssueBoardWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.issueboardwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueBoardWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueBoardWebServerApplication.class, args);
+	}
+
+}
---
+++ IssueBoardWebServer/src/test/java/com/example/issueboardwebserver/IssueBoardWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issueboardwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueBoardWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
